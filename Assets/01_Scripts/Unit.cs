88c9f414c5ade46f49fb87e5129ad182d3900b80
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-12-18 10:41:57 +0900

    Revert "Done"
    This reverts commit 8423ee2942d7194310831131524cbce937c5f621.

--- Assets/01_Scripts/Unit.cs
+++ Assets/01_Scripts/Unit.cs
@@ -15,7 +15,6 @@ public class Unit : NetworkBehaviour
     [Networked, OnChangedRender(nameof(HPChanged))]
     public float HP { get; set; } = 100f; // 서버 상에서 동기화
     public UnitType type;
-    [SerializeField] private TextMeshPro _HPBar;
 
     [SerializeField] private TextMeshPro _HPBar;
 
@@ -184,10 +183,7 @@ public class Unit : NetworkBehaviour
             Invoke(nameof(DisableAttackCollider), 1.0f); // 1.0초 후 비활성화
         }
     }
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
     void EnableAttackCollider()
     {
         _boxCollider2D.enabled = true;
@@ -201,11 +197,7 @@ public class Unit : NetworkBehaviour
     void HPChanged()
     {
         Debug.Log($"Health changed to: {HP} :" + gameObject.name);
-<<<<<<< Updated upstream
         
-=======
-
->>>>>>> Stashed changes
         _HPBar.text = HP.ToString();
 
         if (HP <= 0)
